b2f855c099da90bcb0c7b520b249b9b8f9db0e77
ltei <xatofr35@gmail.com>
2020-01-22 20:32:19 +0100

    update

--- audiodownloader/src/test/kotlin/com/ltei/audiodownloader/Test.kt
+++ audiodownloader/src/test/kotlin/com/ltei/audiodownloader/Test.kt
@@ -4,8 +4,10 @@ import com.google.gson.Gson
 import com.google.gson.GsonBuilder
 import com.ltei.audiodownloader.model.DownloadProgressInterceptor
 import com.ltei.audiodownloader.model.audiosource.AudioSourceUrl
+import com.ltei.audiodownloader.web.jamendo.Boost
 import com.ltei.audiodownloader.web.jamendo.Format
 import com.ltei.audiodownloader.web.jamendo.JamendoClient
+import com.ltei.audiodownloader.web.jamendo.Order
 import org.junit.Test
 import java.io.File
 
@@ -13,15 +15,22 @@ class Test {
 
     @Test
     fun test() {
-        val artists = JamendoClient.getArtist(nameSearch = "abi").execute().body()!!
-        val tracks = JamendoClient.getTrack(artistId = artists.results.first().id.toInt()).execute().body()!!
+//        val artists = JamendoClient.getArtist(nameSearch = "abi").execute().body()!!
+//        val tracks = JamendoClient.getTrack(artistId = artists.results.first().id.toInt()).execute().body()!!
+//        println(GsonBuilder().setPrettyPrinting().create().toJson(tracks))
+//        val url = AudioSourceUrl.parse(tracks.results.first().audioDownload)!!
+//        url.downloadTo(File("test.mp3"), interceptor = object : DownloadProgressInterceptor {
+//            override fun onProgress(progress: Long, total: Long) {
+//                println(progress)
+//            }
+//        })
+
+        val tracks = JamendoClient.getTrack(
+            xArtist = "Kanye West",
+            order = Order.BuzzRate,
+            boost = Boost.BuzzRate
+        ).execute().body()!!
         println(GsonBuilder().setPrettyPrinting().create().toJson(tracks))
-        val url = AudioSourceUrl.parse(tracks.results.first().audioDownload)!!
-        url.downloadTo(File("test.mp3"), interceptor = object : DownloadProgressInterceptor {
-            override fun onProgress(progress: Long, total: Long) {
-                println(progress)
-            }
-        })
     }
 
 }
